d85b8fb6766c71e0df29a28e6d746a7ac6a1ceb2
Gaurav Kumar <gauravraaz2005@gmail.com>
2026-09-20 20:13:42 +0530

    Fix trailing slash causing 308 redirect

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: (import.meta.env.VITE_API_TARGET || '') + '/api',
+  baseURL: (import.meta.env.VITE_API_TARGET?.replace(/\/+$/, '') || '') + '/api',
   withCredentials: true, // important if using cookies for auth
 });
 
